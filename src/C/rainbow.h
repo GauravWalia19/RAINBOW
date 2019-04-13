9ab02fe94095cb3d316ae80a2c498c908ded35f1
GauravWalia19 <gauravwalia019@gmail.com>
2019-04-13 15:01:50 +0530

    completed formatting with colors

--- src/C/rainbow.h
+++ src/C/rainbow.h
@@ -387,44 +387,205 @@ FOR TESTING
 // Display the **over line dark blue** color on screen
 #define OLDBLUE "\x1B[53;34m" 		        // OVER LINE
 
+
 /**************************
  *      LIGHT CYAN        *
  **************************/
+// Display the **bold light cyan** color on screen
+#define BLCYAN "\x1B[1;96m"                 // BOLD
+
+// Display the **italic light cyan** color on screen
+#define ILCYAN "\x1B[3;96m"                 // ITALIC
+
+// Display the **underline light cyan** color on screen
+#define ULLCYAN "\x1B[4;96m"                // UNDERLINE
+
+// Display the **blink light cyan** color on screen
+#define BKLCYAN "\x1B[5;96m"                // BLINK
+
+// Display the **cut light cyan** color on screen
+#define CLCYAN "\x1B[9;96m"                 // CUT
+
+// Display the **double line light cyan** color on screen
+#define DLLCYAN "\x1B[21;96m"               // DOUBLE LINE
+
+// Display the **over line light cyan** color on screen
+#define OLLCYAN "\x1B[53;96m"               // OVER LINE
+
+
 /**************************
  *      DARK CYAN         *
  **************************/
+// Display the **bold dark cyan** color text on screen
+#define BDCYAN "\x1B[1;36m" 		        // BOLD
+
+// Display the **italic dark cyan** color text on screen
+#define IDCYAN "\x1B[3;36m" 		        // ITALIC
+
+// Display the **under line dark cyan** color text on screen
+#define ULDCYAN "\x1B[4;36m" 		        // UNDER LINE
+
+// Display the **blink dark cyan** color text on screen
+#define BKDCYAN "\x1B[5;36m" 		        // BLINK
+
+// Display the **cut dark cyan** color text on screen
+#define CDCYAN "\x1B[9;36m" 		        // CUT
+
+// Display the **double line dark cyan** color text on screen
+#define DLDCYAN "\x1B[21;36m" 		        // DOUBLE LINE
+
+// Display the **over line dark cyan** color text on screen
+#define OLDCYAN "\x1B[53;36m" 		        // OVER LINE
+
+
 /**************************
  *          ORANGE        *
  **************************/
+// Display **bold orange color** text on screen
+#define BORANGE "\x1B[1;33m" 		        // BOLD
+
+// Display **italic orange color** text on screen
+#define IORANGE "\x1B[3;33m" 		        // ITALIC
+
+// Display **under line orange color** text on screen
+#define ULORANGE "\x1B[4;33m" 		        // UNDER LINE
+
+// Display **blink orange color** text on screen
+#define BKORANGE "\x1B[5;33m" 		        // BLINK
+
+// Display **strikethrough orange color** text on screen
+#define CORANGE "\x1B[9;33m" 		        // CUT
+
+// Display **double line orange color** text on screen
+#define DLORANGE "\x1B[21;33m" 		        // DOUBLE LINE
+
+// Display **over line orange color** text on screen
+#define OLORANGE "\x1B[53;33m" 		        // OVER LINE
+
+
 /**************************
  *          BLACK         *
  **************************/
+// Display the **bold black color** text on screen
+#define BBLACK "\x1B[1;30m" 		        // BOLD
+
+// Display the **italic black color** text on screen
+#define IBLACK "\x1B[3;30m" 		        // ITALIC
+
+// Display the **under line black color** text on screen
+#define ULBLACK "\x1B[4;30m" 		        // UNDER LINE
+
+// Display the **blinking black color** text on screen
+#define BKBLACK "\x1B[5;30m" 		        // BLINK
+
+// Display the **strikethrough black color** text on screen
+#define CBLACK "\x1B[9;30m" 		        // CUT
+
+// Display the **double line black color** text on screen
+#define DLBLACK "\x1B[21;30m" 		        // DOUBLE LINE
+
+// Display the **over line black color** text on screen
+#define OLBLACK "\x1B[53;30m" 		        // OVER LINE
+
+
 /**************************
  *          MAGENTA       *
  **************************/
+// Display **bold magenta color** text on screen
+#define BMAGENTA "\x1B[1;35m" 	            // BOLD
+
+// Display **italic magenta color** text on screen
+#define IMAGENTA "\x1B[3;35m" 	            // ITALIC
+
+// Display **under line magenta color** text on screen
+#define ULMAGENTA "\x1B[4;35m" 	            // UNDER LINE
+
+// Display **blink magenta color** text on screen
+#define BKMAGENTA "\x1B[5;35m" 	            // BLINK
+
+// Display **cut magenta color** text on screen
+#define CMAGENTA "\x1B[9;35m" 	            // CUT
+
+// Display **double line magenta color** text on screen
+#define DLMAGENTA "\x1B[21;35m" 	        // DOUBLE LINE
+
+// Display **over line magenta color** text on screen
+#define OLMAGENTA "\x1B[53;35m" 	        // OVER LINE
+
+
 /**************************
  *          YELLOW        *
  **************************/
+// Display **bold yellow color** text on screen
+#define BYELLOW "\x1B[1;92m"               // BOLD
+
+// Display **italic yellow color** text on screen
+#define IYELLOW "\x1B[3;92m"               // ITALIC
+
+// Display **under line yellow color** text on screen
+#define ULYELLOW "\x1B[4;92m"               // UNDER LINE
+
+// Display **blink yellow color** text on screen
+#define BKYELLOW "\x1B[5;92m"               // BLINK
+
+// Display **cut yellow color** text on screen
+#define CYELLOW "\x1B[9;92m"               // CUT
+
+// Display **double line yellow color** text on screen
+#define DLYELLOW "\x1B[21;92m"             // DOUBLE LINE
+
+// Display **over line yellow color** text on screen
+#define OLYELLOW "\x1B[53;92m"             // OVER LINE
+
+
 /**************************
  *          LEMON         *
  **************************/
+// Display the **bold lemon color** text on screen
+#define BLEMON "\x1B[1;93m"                // BOLD
+
+// Display the **italic lemon color** text on screen
+#define ILEMON "\x1B[3;93m"                // ITALIC
+
+// Display the **under line lemon color** text on screen
+#define ULLEMON "\x1B[4;93m"                // UNDER LINE
+
+// Display the **blink lemon color** text on screen
+#define BKLEMON "\x1B[5;93m"                // BLINK
+
+// Display the **cut lemon color** text on screen
+#define CLEMON "\x1B[9;93m"                // CUT
+
+// Display the **double line lemon color** text on screen
+#define DLLEMON "\x1B[21;93m"                // DOUBLE LINE
+
+// Display the **over line lemon color** text on screen
+#define OLLEMON "\x1B[53;93m"                // OVER LINE
+
+
 /**************************
  *          PINK          *
  **************************/
+// Display the **bold pink color** text on screen
+#define BPINK "\x1B[1;95m"                 // BOLD
 
+// Display the **italic pink color** text on screen
+#define IPINK "\x1B[3;95m"                 // ITALIC
 
-#define BYELLOW "\x1B[1;33m"//BOLD YELLOW
-#define IYELLOW "\x1B[3;33m"   //ITALIC YELLOW
-#define UYELLOW "\x1B[4;33m"		//UNDERLINE YELLOW
-#define BBLUE "\x1B[1;34m"				//BOLD BLUE
-#define IBLUE "\x1B[3;34m"			//ITALIC BLUE
-#define UBLUE "\x1B[4;34m" 	//UNDERLINE BLUE
-#define BMAGENTA "\x1B[1;35m" //BOLD MAGENTA
-#define IMAGENTA "\x1B[3;35m"		//ITALIC MAGENTA
-#define UMAGENTA "\x1B[4;35m" //UNDERLINE MAGENTA
-#define BCYAN "\x1B[1;36m"		//BOLD CYAN
-#define ICYAN "\x1B[3;36m" 		//ITALIC CYAN
-#define UCYAN "\x1B[4;36m" 		//UNDERLINE CYAN
+// Display the **under line pink color** text on screen
+#define ULPINK "\x1B[4;95m"                 // UNDER LINE
+
+// Display the **blink pink color** text on screen
+#define BKPINK "\x1B[5;95m"                 // BLINK
+
+// Display the **cut pink color** text on screen
+#define CPINK "\x1B[9;95m"                 // CUT
+
+// Display the **double line pink color** text on screen
+#define DLPINK "\x1B[21;95m"                 // DOUBLE LINE
+
+// Display the **over line pink color** text on screen
+#define OLPINK "\x1B[53;95m"                 // OVER LINE
 
 
 //FORMATTING WITH BACKGROUND
