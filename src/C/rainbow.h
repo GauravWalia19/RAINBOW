549351dde4bac44276288643dfb03dedc77469a7
GauravWalia19 <gauravwalia019@gmail.com>
2019-04-13 19:24:16 +0530

    completed formatting with background file

--- src/C/rainbow.h
+++ src/C/rainbow.h
@@ -3,11 +3,11 @@ B       1; - bold
 I       3; - italic
 UL      4; - underline
 BK      5; - blink
-H       8; - hidden
 C        9; - cut
 DL       21; - double line
 OL       53; - overline
 
+H       8; - hidden
 FOR TESTING
 #define TEST "\x1B[107m"
 * unknown colors
@@ -16,109 +16,8 @@ FOR TESTING
 */
 // #define TEST "\x1B[106m"
 
-#include "rainformat.h"         // contains formatting text and highlight types
-#include "raincolor.h"          // contains all text colors
-#include "rainbackground.h"     // contains all background colors
-#include "rainformatcolor.h"    // contains formatting with colors
-
-/************************************************************************************
- *                              FORMATTING WITH BACKGROUND                          *
- ************************************************************************************/
-
-/********************************
- *      DARK GREEN BACKGROUND   *
- ********************************/
-// Display the **bold text dark green background** color on screen
-#define BBGDGREEN "\x1B[1;40m"             // DARK GREEN BACKGROUND WITH BOLD TEXT
-
-// Display the **italic text dark green background** color on screen
-#define IBGDGREEN "\x1B[3;40m"             // DARK GREEN BACKGROUND WITH ITALIC TEXT
-
-// Display the **underline text dark green background** color on screen
-#define ULBGDGREEN "\x1B[4;40m"             // DARK GREEN BACKGROUND WITH UNDERLINE TEXT
-
-// Display the **blink text dark green background** color on screen
-#define BKBGDGREEN "\x1B[5;40m"             // DARK GREEN BACKGROUND WITH BLINK TEXT
-
-// Display the **strikethrough text dark green background** color on screen
-#define CBGDGREEN "\x1B[9;40m"             // DARK GREEN BACKGROUND WITH STRIKETHROUGH TEXT
-
-// Display the **double line text dark green background** color on screen
-#define DLBGDGREEN "\x1B[21;40m"             // DOUBLE LINE DARK GREEN BACKGROUND
-
-// Display the **over line text dark green background** color on screen
-#define OLBGDGREEN "\x1B[53;40m"             // OVER LINE DARK GREEN BACKGROUND
-
-
-/********************************
- *     LIGHT GREEN BACKGROUND   *
- ********************************/
-// Display the **bold light green background** color on screen
-#define BBGLGREEN "\x1B[1;42m"	            // BOLD LIGHT GREEN BACKGROUND
-
-// Display the **italic light green background** color on screen
-#define IBGLGREEN "\x1B[3;42m"	            // ITALIC LIGHT GREEN BACKGROUND
-
-// Display the **underline light green background** color on screen
-#define ULBGLGREEN "\x1B[4;42m"	            // UNDERLINE LIGHT GREEN BACKGROUND
-
-// Display the **blink light green background** color on screen
-#define BKBGLGREEN "\x1B[5;42m"	            // BLINK LIGHT GREEN BACKGROUND
-
-// Display the **strikethrough light green background** color on screen
-#define CBGLGREEN "\x1B[9;42m"	            // CUT LIGHT GREEN BACKGROUND
-
-// Display the **double line light green background** color on screen
-#define DLBGLGREEN "\x1B[21;42m"	            // DOUBLE LINE LIGHT GREEN BACKGROUND
-
-// Display the **over line light green background** color on screen
-#define OLBGLGREEN "\x1B[53;42m"	            // OVER LINE LIGHT GREEN BACKGROUND
-
-
-/********************************
- *    MEDIUM GREEN BACKGROUND   *
- ********************************/
-
-/********************************
- *      DARK RED BACKGROUND     *
- ********************************/
-
-/********************************
- *      LIGHT RED BACKGROUND    *
- ********************************/
-
-/********************************
- *     DARK ORANGE BACKGROUND   *
- ********************************/
-
-/********************************
- *     LIGHT ORANGE BACKGROUND   *
- ********************************/
-
-/********************************
- *     LIGHT BLUE BACKGROUND   *
- ********************************/
-
-/********************************
- *     LIGHT WHITE BACKGROUND   *
- ********************************/
-
-/********************************
- *     DARK WHITE BACKGROUND   *
- ********************************/
-
-/********************************
- *        LEMON BACKGROUND      *
- ********************************/
-
-/********************************
- *     MID PINK BACKGROUND   *
- ********************************/
-
-/********************************
- *     DARK PINK BACKGROUND   *
- ********************************/
-
-/********************************
- *     LIGHT PINK BACKGROUND   *
- ********************************/
+#include "rainformat.h"             // contains formatting text and highlight types
+#include "raincolor.h"              // contains all text colors
+#include "rainbackground.h"         // contains all background colors
+#include "rainformatcolor.h"        // contains formatting with colors
+#include "rainformatbackground.h"   // contains formatting with background
---
+++ src/C/rainformatbackground.h
@@ -0,0 +1,350 @@
+/************************************************************************************
+ *                              FORMATTING WITH BACKGROUND                          *
+ ************************************************************************************/
+
+/********************************
+ *      DARK GREEN BACKGROUND   *
+ ********************************/
+// Display the **bold text with dark green background** color on screen
+#define BBGDGREEN "\x1B[1;40m"                  // DARK GREEN BACKGROUND WITH BOLD TEXT
+
+// Display the **italic text with dark green background** color on screen
+#define IBGDGREEN "\x1B[3;40m"                  // DARK GREEN BACKGROUND WITH ITALIC TEXT
+
+// Display the **underline text with dark green background** color on screen
+#define ULBGDGREEN "\x1B[4;40m"                 // DARK GREEN BACKGROUND WITH UNDERLINE TEXT
+
+// Display the **blink text with dark green background** color on screen
+#define BKBGDGREEN "\x1B[5;40m"                 // DARK GREEN BACKGROUND WITH BLINK TEXT
+
+// Display the **strikethrough text with dark green background** color on screen
+#define CBGDGREEN "\x1B[9;40m"                  // DARK GREEN BACKGROUND WITH STRIKETHROUGH TEXT
+
+// Display the **double line text with dark green background** color on screen
+#define DLBGDGREEN "\x1B[21;40m"                // DOUBLE LINE DARK GREEN BACKGROUND
+
+// Display the **over line text with dark green background** color on screen
+#define OLBGDGREEN "\x1B[53;40m"                // OVER LINE DARK GREEN BACKGROUND
+
+
+/********************************
+ *     LIGHT GREEN BACKGROUND   *
+ ********************************/
+// Display the **bold text with light green background** color on screen
+#define BBGLGREEN "\x1B[1;42m"	                // BOLD LIGHT GREEN BACKGROUND
+
+// Display the **italic text with light green background** color on screen
+#define IBGLGREEN "\x1B[3;42m"	                // ITALIC LIGHT GREEN BACKGROUND
+
+// Display the **underline text with light green background** color on screen
+#define ULBGLGREEN "\x1B[4;42m"	                // UNDERLINE LIGHT GREEN BACKGROUND
+
+// Display the **blink text with light green background** color on screen
+#define BKBGLGREEN "\x1B[5;42m"	                // BLINK LIGHT GREEN BACKGROUND
+
+// Display the **strikethrough text with light green background** color on screen
+#define CBGLGREEN "\x1B[9;42m"	                // CUT LIGHT GREEN BACKGROUND
+
+// Display the **double line text with light green background** color on screen
+#define DLBGLGREEN "\x1B[21;42m"	            // DOUBLE LINE LIGHT GREEN BACKGROUND
+
+// Display the **over line text with light green background** color on screen
+#define OLBGLGREEN "\x1B[53;42m"	            // OVER LINE LIGHT GREEN BACKGROUND
+
+
+/********************************
+ *    MEDIUM GREEN BACKGROUND   *
+ ********************************/
+// Display the **bold text with medium green color background** on screen
+#define BBGMGREEN "\x1B[1;100m"                 // BOLD TEXT WITH MID GREEN BACKGROUND
+
+// Display the **italic text with medium green color background** on screen
+#define IBGMGREEN "\x1B[3;100m"                 // ITALIC TEXT WITH MID GREEN BACKGROUND
+
+// Display the **underline text with medium green color background** on screen
+#define ULBGMGREEN "\x1B[4;100m"                // UNDERLINE TEXT WITH MID GREEN BACKGROUND
+
+// Display the **blink text with medium green color background** on screen
+#define BKBGMGREEN "\x1B[5;100m"                // BLINK TEXT WITH MID GREEN BACKGROUND
+
+// Display the **cut text with medium green color background** on screen
+#define CBGMGREEN "\x1B[9;100m"                 // CUT TEXT WITH MID GREEN BACKGROUND
+
+// Display the **double line text with medium green color background** on screen
+#define DLBGMGREEN "\x1B[21;100m"               // DOUBLE LINE TEXT WITH MID GREEN BACKGROUND
+
+// Display the **over line text with medium green color background** on screen
+#define OLBGMGREEN "\x1B[53;100m"               // OVERLINE TEXT WITH MID GREEN BACKGROUND
+
+
+/********************************
+ *      DARK RED BACKGROUND     *
+ ********************************/
+// Display the **bold text with dark red color background** on screen
+#define BBGDRED "\x1B[1;41m"		               // BOLD TEXT WITH DARK RED BACKGROUND
+
+// Display the **italic text with dark red color background** on screen
+#define IBGDRED "\x1B[3;41m"		               // ITALIC TEXT WITH DARK RED BACKGROUND
+
+// Display the **underline text with dark red color background** on screen
+#define ULBGDRED "\x1B[4;41m"		               // UNDERLINE TEXT WITH DARK RED BACKGROUND
+
+// Display the **blink text with dark red color background** on screen
+#define BKBGDRED "\x1B[5;41m"		               // BLINK TEXT WITH DARK RED BACKGROUND
+
+// Display the **cut text with dark red color background** on screen
+#define CBGDRED "\x1B[9;41m"		               // CUT TEXT WITH DARK RED BACKGROUND
+
+// Display the **double line text with dark red color background** on screen
+#define DLBGDRED "\x1B[21;41m"		               // DOUBLE LINE TEXT WITH DARK RED BACKGROUND
+
+// Display the **overline text with dark red color background** on screen
+#define OLBGDRED "\x1B[53;41m"		               // OVERLINE TEXT WITH DARK RED BACKGROUND
+
+
+/********************************
+ *      LIGHT RED BACKGROUND    *
+ ********************************/
+// Display the **bold text with light red color background** on screen
+#define BBGLRED "\x1B[1;101m"                       // BOLD TEXT WITH LIGHT RED BACKGROUND
+
+// Display the **italic text with light red color background** on screen
+#define IBGLRED "\x1B[3;101m"                       // ITALIC TEXT WITH LIGHT RED BACKGROUND
+
+// Display the **underline text with light red color background** on screen
+#define ULBGLRED "\x1B[4;101m"                      // UNDERLINE TEXT WITH LIGHT RED BACKGROUND
+
+// Display the **blink text with light red color background** on screen
+#define BKBGLRED "\x1B[5;101m"                      // BLINK TEXT WITH LIGHT RED BACKGROUND
+
+// Display the **cut text with light red color background** on screen
+#define CBGLRED "\x1B[9;101m"                       // CUT TEXT WITH LIGHT RED BACKGROUND
+
+// Display the **double line text with light red color background** on screen
+#define DLBGLRED "\x1B[21;101m"                     // DOUBLE LINE TEXT WITH LIGHT RED BACKGROUND
+
+// Display the **over line text with light red color background** on screen
+#define OLBGLRED "\x1B[53;101m"                     // OVER LINE TEXT WITH LIGHT RED BACKGROUND
+
+
+/********************************
+ *     DARK ORANGE BACKGROUND   *
+ ********************************/
+// Display the **bold text with dark orange color background** on screen
+#define BBGDORANGE "\x1B[1;43m"	                    // BOLD TEXT WITH DARK ORANGE BACKGROUND
+
+// Display the **italic text with dark orange color background** on screen
+#define IBGDORANGE "\x1B[3;43m"	                    // ITALIC TEXT WITH DARK ORANGE BACKGROUND
+
+// Display the **underline text with dark orange color background** on screen
+#define ULBGDORANGE "\x1B[4;43m"	                // UNDERLINE TEXT WITH DARK ORANGE BACKGROUND
+
+// Display the **blink text with dark orange color background** on screen
+#define BKBGDORANGE "\x1B[5;43m"	                // BLINK TEXT WITH DARK ORANGE BACKGROUND
+
+// Display the **cut text with dark orange color background** on screen
+#define CBGDORANGE "\x1B[9;43m"	                    // CUT TEXT WITH DARK ORANGE BACKGROUND
+
+// Display the **double line text with dark orange color background** on screen
+#define DLBGDORANGE "\x1B[21;43m"	                // DOUBLE LINE TEXT WITH DARK ORANGE BACKGROUND
+
+// Display the **overline text with dark orange color background** on screen
+#define OLBGDORANGE "\x1B[53;43m"	                // OVER LINE TEXT WITH DARK ORANGE BACKGROUND
+
+/********************************
+ *     LIGHT ORANGE BACKGROUND   *
+ ********************************/
+// Display the **bold text with light orange color background** on screen
+#define BBGLORANGE "\x1B[1;102m"                    // BOLD TEXT WITH LIGHT ORANGE BACKGROUND
+
+// Display the **italic text with light orange color background** on screen
+#define IBGLORANGE "\x1B[3;102m"                    // ITALIC TEXT WITH LIGHT ORANGE BACKGROUND
+
+// Display the **underline text with light orange color background** on screen
+#define ULBGLORANGE "\x1B[4;102m"                   // UNDERLINE TEXT WITH LIGHT ORANGE BACKGROUND
+
+// Display the **blink text with light orange color background** on screen
+#define BKBGLORANGE "\x1B[5;102m"                   // BLINK TEXT WITH LIGHT ORANGE BACKGROUND
+
+// Display the **cut text with light orange color background** on screen
+#define CBGLORANGE "\x1B[9;102m"                    // CUT TEXT WITH LIGHT ORANGE BACKGROUND
+
+// Display the **double line text with light orange color background** on screen
+#define DLBGLORANGE "\x1B[21;102m"                  // DOUBLE LINE TEXT WITH LIGHT ORANGE BACKGROUND
+
+// Display the **overline text with light orange color background** on screen
+#define OLBGLORANGE "\x1B[53;102m"                  // OVERLINE TEXT WITH LIGHT ORANGE BACKGROUND
+
+
+/********************************
+ *     LIGHT BLUE BACKGROUND   *
+ ********************************/
+// Display the **bold text with light blue color background** on screen
+#define BBGLBLUE "\x1B[1;44m"		                // BOLD TEXT WITH LIGHT BLUE BACKGROUND
+
+// Display the **italic text with light blue color background** on screen
+#define IBGLBLUE "\x1B[3;44m"		                // ITALIC TEXT WITH LIGHT BLUE BACKGROUND
+
+// Display the **underline text with light blue color background** on screen
+#define ULBGLBLUE "\x1B[4;44m"		                // UNDERLINE TEXT WITH LIGHT BLUE BACKGROUND
+
+// Display the **blink text with light blue color background** on screen
+#define BKBGLBLUE "\x1B[5;44m"		                // BLINK TEXT WITH LIGHT BLUE BACKGROUND
+
+// Display the **cut text with light blue color background** on screen
+#define CBGLBLUE "\x1B[9;44m"		                // CUT TEXT WITH LIGHT BLUE BACKGROUND
+
+// Display the **double line text with light blue color background** on screen
+#define DLBGLBLUE "\x1B[21;44m"		                // DOUBLE LINE TEXT WITH LIGHT BLUE BACKGROUND
+
+// Display the **overline text with light blue color background** on screen
+#define OLBGLBLUE "\x1B[53;44m"		                // OVERLINE TEXT WITH LIGHT BLUE BACKGROUND
+
+
+/********************************
+ *     LIGHT WHITE BACKGROUND   *
+ ********************************/
+// Display the **bold text with light white color background** on screen
+#define BBGLWHITE "\x1B[1;47m"		                   // BOLD TEXT WITH LIGHT WHITE BACKGROUND
+
+// Display the *italic text with light white color background** on screen
+#define IBGLWHITE "\x1B[3;47m"		                   // ITALIC TEXT WITH LIGHT WHITE BACKGROUND
+
+// Display the **underline text with light white color background** on screen
+#define ULBGLWHITE "\x1B[4;47m"		                   // UNDERLINE TEXT WITH LIGHT WHITE BACKGROUND
+
+// Display the **blink text with light white color background** on screen
+#define BKBGLWHITE "\x1B[5;47m"		                   // BLINK TEXT WITH LIGHT WHITE BACKGROUND
+
+// Display the **cut text with light white color background** on screen
+#define CBGLWHITE "\x1B[9;47m"		                   // CUT TEXT WITH LIGHT WHITE BACKGROUND
+
+// Display the **double line text with light white color background** on screen
+#define DLBGLWHITE "\x1B[21;47m"		               // DOUBLE LINE TEXT WITH LIGHT WHITE BACKGROUND
+
+// Display the **over line text with light white color background** on screen
+#define OLBGLWHITE "\x1B[53;47m"		               // OVER LINE TEXT WITH LIGHT WHITE BACKGROUND
+
+
+/********************************
+ *     DARK WHITE BACKGROUND   *
+ ********************************/
+// Display the **bold text with dark white color background** on screen
+#define BBGDWHITE "\x1B[1;107m"                         // BOLD TEXT WITH DARK WHITE BACKGROUND
+
+// Display the **italic text with dark white color background** on screen
+#define IBGDWHITE "\x1B[3;107m"                         // ITALIC TEXT WITH DARK WHITE BACKGROUND
+
+// Display the **underline text with dark white color background** on screen
+#define ULBGDWHITE "\x1B[4;107m"                         // UNDERLINE TEXT WITH DARK WHITE BACKGROUND
+
+// Display the **blink text with dark white color background** on screen
+#define BKBGDWHITE "\x1B[5;107m"                         // BLINK TEXT WITH DARK WHITE BACKGROUND
+
+// Display the **cut text with dark white color background** on screen
+#define CBGDWHITE "\x1B[9;107m"                         // CUT TEXT WITH DARK WHITE BACKGROUND
+
+// Display the **double line text with dark white color background** on screen
+#define DLBGDWHITE "\x1B[21;107m"                         // DOUBLE LINE TEXT WITH DARK WHITE BACKGROUND
+
+// Display the **over line text with dark white color background** on screen
+#define OLBGDWHITE "\x1B[53;107m"                         // OVER LINE TEXT WITH DARK WHITE BACKGROUND
+
+
+/********************************
+ *        LEMON BACKGROUND      *
+ ********************************/
+// Display the **bold text with lemon background color** on screen
+#define BBGLEMON "\x1B[1;103m"                              // BOLD TEXT LINE LEMON BACKGROUND
+
+// Display the **italic text with lemon background color** on screen
+#define IBGLEMON "\x1B[3;103m"                              // ITALIC TEXT LINE LEMON BACKGROUND
+
+// Display the **underline text with lemon background color** on screen
+#define ULBGLEMON "\x1B[4;103m"                              // UNDERLINE TEXT LINE LEMON BACKGROUND
+
+// Display the **blink text with lemon background color** on screen
+#define BKBGLEMON "\x1B[5;103m"                              // BLINK TEXT LINE LEMON BACKGROUND
+
+// Display the **cut text with lemon background color** on screen
+#define CBGLEMON "\x1B[9;103m"                              // CUT TEXT LINE LEMON BACKGROUND
+
+// Display the **double line text with lemon background color** on screen
+#define DLBGLEMON "\x1B[21;103m"                              // DOUBLE LINE TEXT LINE LEMON BACKGROUND
+
+// Display the **over line text with lemon background color** on screen
+#define OLBGLEMON "\x1B[53;103m"                              // OVER LINE TEXT LINE LEMON BACKGROUND
+
+
+/********************************
+ *     MID PINK BACKGROUND   *
+ ********************************/
+// Display the **bold text with medium pink background** on screen
+#define BBGMPINK "\x1B[1;104m"                                 // BOLD TEXT WITH MID PINK BACKGROUND
+
+// Display the **italic text with medium pink background** on screen
+#define IBGMPINK "\x1B[3;104m"                                 // ITALIC TEXT WITH MID PINK BACKGROUND
+
+// Display the **underline text with medium pink background** on screen
+#define ULBGMPINK "\x1B[4;104m"                                 // UNDERLINE TEXT WITH MID PINK BACKGROUND
+
+// Display the **blink text with medium pink background** on screen
+#define BKBGMPINK "\x1B[5;104m"                                 // BLINK TEXT WITH MID PINK BACKGROUND
+
+// Display the **cut text with medium pink background** on screen
+#define CBGMPINK "\x1B[9;104m"                                 // CUT TEXT WITH MID PINK BACKGROUND
+
+// Display the **doubleline text with medium pink background** on screen
+#define DLBGMPINK "\x1B[21;104m"                                 // DOUBLE LINE TEXT WITH MID PINK BACKGROUND
+
+// Display the **overline text with medium pink background** on screen
+#define OLBGMPINK "\x1B[53;104m"                                 // OVERLINE TEXT WITH MID PINK BACKGROUND
+
+/********************************
+ *     DARK PINK BACKGROUND   *
+ ********************************/
+// Display the **bold text with dark pink color background** on screen
+#define BBGDPINK "\x1B[1;105m"                        // BOLD TEXT WITH DARK PINK BACKGROUND
+
+// Display the **italic text with dark pink color background** on screen
+#define IBGDPINK "\x1B[3;105m"                        // ITALIC TEXT WITH DARK PINK BACKGROUND
+
+// Display the **underline text with dark pink color background** on screen
+#define ULBGDPINK "\x1B[4;105m"                        // UNDERLINE TEXT WITH DARK PINK BACKGROUND
+
+// Display the **blink text text with dark pink color background** on screen
+#define BKBGDPINK "\x1B[5;105m"                        // BLINK TEXT WITH DARK PINK BACKGROUND
+
+// Display the **cut text with dark pink color background** on screen
+#define CBGDPINK "\x1B[9;105m"                        // CUT TEXT WITH DARK PINK BACKGROUND
+
+// Display the **double line text with dark pink color background** on screen
+#define DLBGDPINK "\x1B[21;105m"                        // DOUBLE LINE TEXT WITH DARK PINK BACKGROUND
+
+// Display the **over line  text with dark pink color background** on screen
+#define OLBGDPINK "\x1B[53;105m"                        //OVERLINE TEXT WITH DARK PINK BACKGROUND
+
+
+/********************************
+ *     LIGHT PINK BACKGROUND   *
+ ********************************/
+// Display the **text with light pink color background** on screen
+#define BBGLPINK "\x1B[1;106m"                                // BOLD TEXT WITH LIGHT PINK BACKGROUND
+
+// Display the **text with light pink color background** on screen
+#define IBGLPINK "\x1B[3;106m"                                // ITALIC TEXT WITH LIGHT PINK BACKGROUND
+
+// Display the **text with light pink color background** on screen
+#define ULBGLPINK "\x1B[4;106m"                               // UNDERLINE TEXT WITH LIGHT PINK BACKGROUND
+
+// Display the **text with light pink color background** on screen
+#define BKBGLPINK "\x1B[5;106m"                               // BLINK TEXT WITH LIGHT PINK BACKGROUND
+
+// Display the **text with light pink color background** on screen
+#define CBGLPINK "\x1B[9;106m"                                // CUT TEXT WITH LIGHT PINK BACKGROUND
+
+// Display the **text with light pink color background** on screen
+#define DLBGLPINK "\x1B[21;106m"                              // DOUBLE LINE TEXT WITH LIGHT PINK BACKGROUND
+
+// Display the **text with light pink color background** on screen
+#define OLBGLPINK "\x1B[5 3;106m"                            // OVER LINE TEXT WITH LIGHT PINK BACKGROUND
