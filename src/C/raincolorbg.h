515d4f396d016296e9cf6edd82e8a421457a2643
GauravWalia19 <gauravwalia019@gmail.com>
2019-04-26 02:41:31 +0530

    added 5 sub modules

--- src/C/raincolorbg.h
+++ src/C/raincolorbg.h
@@ -1,102 +1,301 @@
 /********************
  *      LGREEN
  ********************/
-
 // Display the LIGHT GREEN text with DARK GREEN background
 #define LGREEN_BGDGREEN "\x1B[32;40m" 		             
 
 // Display the LIGHT GREEN text with LIGHT GREEN background
 #define LGREEN_BGLGREEN "\x1B[32;42m" 		              
+
+// Display the LIGHT GREEN text with MEDIUM GREEN background
 #define LGREEN_BGMGREEN "\x1B[32;100m"
-#define LGREEN_BGDRED "\x1B[32;41m" 		             
-#define LGREEN_BGLRED "\x1B[32;101m" 		             
-#define LGREEN_BGDORANGE "\x1B[32;43m" 		             
-#define LGREEN_BGLORANGE "\x1B[32;102m" 		         
-#define LGREEN_BGLBLUE "\x1B[32;44m" 		             
-#define LGREEN_BGLWHITE "\x1B[32;47m" 		             
-#define LGREEN_BGDWHITE "\x1B[32;107m" 		             
-#define LGREEN_BGLEMON "\x1B[32;103m" 		             
-#define LGREEN_BGMPINK "\x1B[32;104m" 		             
-#define LGREEN_BGDPINK "\x1B[32;105m" 		             
+
+// Display the LIGHT GREEN text with DARK RED background
+#define LGREEN_BGDRED "\x1B[32;41m" 		
+
+// Display the LIGHT GREEN text with LIGHT RED background
+#define LGREEN_BGLRED "\x1B[32;101m" 		 
+
+// Display the LIGHT GREEN text with DARK ORANGE background
+#define LGREEN_BGDORANGE "\x1B[32;43m" 		    
+
+// Display the LIGHT GREEN text with LIGHT ORANGE background
+#define LGREEN_BGLORANGE "\x1B[32;102m" 		 
+
+// Display the LIGHT GREEN text with LIGHT BLUE background
+#define LGREEN_BGLBLUE "\x1B[32;44m" 		   
+
+// Display the LIGHT GREEN text with LIGHT WHITE background
+#define LGREEN_BGLWHITE "\x1B[32;47m" 		    
+
+// Display the LIGHT GREEN text with DARK WHITE background
+#define LGREEN_BGDWHITE "\x1B[32;107m" 		  
+
+// Display the LIGHT GREEN text with LEMON background
+#define LGREEN_BGLEMON "\x1B[32;103m" 	
+
+// Display the LIGHT GREEN text with MEDIUM PINK background
+#define LGREEN_BGMPINK "\x1B[32;104m" 		    
+
+// Display the LIGHT GREEN text with DARK PINK background
+#define LGREEN_BGDPINK "\x1B[32;105m" 		  
+
+// Display the LIGHT GREEN text with LIGHT PINK background
 #define LGREEN_BGLPINK "\x1B[32;106m" 		             
+
 
 /********************
  *      MGREEN
  ********************/
-#define MGREEN "\x1B[2m" 		                        // MID GREEN COLOR
+// Display the MEDIUM GREEN text with DARK GREEN background
+#define MGREEN_BGDGREEN "\x1B[2;40m"
+
+// Display the MEDIUM GREEN text with LIGHT GREEN background
+#define MGREEN_BGLGREEN "\x1B[2;42m"
+
+// Display the MEDIUM GREEN text with MEDIUM GREEN background
+#define MGREEN_BGMGREEN "\x1B[2;100m"
+
+// Display the MEDIUM GREEN text with DARK RED background
+#define MGREEN_BGDRED "\x1B[2;41m"
+
+// Display the MEDIUM GREEN text with LIGHT RED background
+#define MGREEN_BGLRED "\x1B[2;101m"
+
+// Display the MEDIUM GREEN text with DARK ORANGE background
+#define MGREEN_BGDORANGE "\x1B[2;43m"
+
+// Display the MEDIUM GREEN text with LIGHT ORANGE background
+#define MGREEN_BGLORANGE "\x1B[2;102m"
+
+// Display the MEDIUM GREEN text with LIGHT BLUE background
+#define MGREEN_BGLBLUE "\x1B[2;44m"
+
+// Display the MEDIUM GREEN text with LIGHT WHITE background
+#define MGREEN_BGLWHITE "\x1B[2;47m"
+
+// Display the MEDIUM GREEN text with DARK WHITE background
+#define MGREEN_BGDWHITE "\x1B[2;107m"
+
+// Display the MEDIUM GREEN text with LEMON background
+#define MGREEN_BGLEMON "\x1B[2;103m"
+
+// Display the MEDIUM GREEN text with MEDIUM PINK background
+#define MGREEN_BGMPINK "\x1B[2;104m"
+
+// Display the MEDIUM GREEN text with DARK PINK background
+#define MGREEN_BGDPINK "\x1B[2;105m"
+
+// Display the MEDIUM GREEN text with LIGHT PINK background
+#define MGREEN_BGLPINK "\x1B[2;106m"
 
 
 /********************
  *      DGREEN
  ********************/
-#define DGREEN "\x1B[90m"                               // DARK GREEN COLOR
+// Display the DARK GREEN text with DARK GREEN background
+#define DGREEN_BGDGREEN "\x1B[90;40m"
+
+// Display the DARK GREEN text with LIGHT GREEN background
+#define DGREEN_BGLGREEN "\x1B[90;42m"
+
+// Display the DARK GREEN text with MEDIUM GREEN background
+#define DGREEN_BGMGREEN "\x1B[90;100m"
+
+// Display the DARK GREEN text with DARK RED background
+#define DGREEN_BGDRED "\x1B[90;41m"
+
+// Display the DARK GREEN text with LIGHT RED background
+#define DGREEN_BGLRED "\x1B[90;101m"
+
+// Display the DARK GREEN text with DARK ORANGE background
+#define DGREEN_BGDORANGE "\x1B[90;43m"
+
+// Display the DARK GREEN text with LIGHT ORANGE background
+#define DGREEN_BGLORANGE "\x1B[90;102m"
+
+// Display the DARK GREEN text with LIGHT BLUE background
+#define DGREEN_BGLBLUE "\x1B[90;44m"
+
+// Display the DARK GREEN text with LIGHT WHITE background
+#define DGREEN_BGLWHITE "\x1B[90;47m"
+
+// Display the DARK GREEN text with DARK WHITE background
+#define DGREEN_BGDWHITE "\x1B[90;107m"
+
+// Display the DARK GREEN text with LEMON background
+#define DGREEN_BGLEMON "\x1B[90;103m"
+
+// Display the DARK GREEN text with MEDIUM PINK background
+#define DGREEN_BGMPINK "\x1B[90;104m"
+
+// Display the DARK GREEN text with DARK PINK background
+#define DGREEN_BGDPINK "\x1B[90;105m"
+
+// Display the DARK GREEN text with LIGHT PINK background
+#define DGREEN_BGLPINK "\x1B[90;106m"
+
 
 /********************
  *      LRED
  ********************/
-#define LRED "\x1B[91m"                                 // LIGHT RED COLOR
+// Display the LIGHT RED text with DARK GREEN background
+#define LRED_BGDGREEN "\x1B[91;40m"
+
+// Display the LIGHT RED text with LIGHT GREEN background
+#define LRED_BGLGREEN "\x1B[91;42m"
+
+// Display the LIGHT RED text with MEDIUM GREEN background
+#define LRED_BGMGREEN "\x1B[91;100m"
+
+// Display the LIGHT RED text with DARK RED background
+#define LRED_BGDRED "\x1B[91;41m"
+
+// Display the LIGHT RED text with LIGHT RED background
+#define LRED_BGLRED "\x1B[91;101m"
+
+// Display the LIGHT RED text with DARK ORANGE background
+#define LRED_BGORANGE "\x1B[91;43m"
+
+// Display the LIGHT RED text with LIGHT ORANGE background
+#define LRED_BGLORANGE "\x1B[91;102m"
+
+// Display the LIGHT RED text with LIGHT BLUE background
+#define LRED_BGLBLUE "\x1B[91;44m"
+
+// Display the LIGHT RED text with LIGHT WHITE background
+#define LRED_BGLWHITE "\x1B[91;47m"
+
+// Display the LIGHT RED text with DARK WHITE background
+#define LRED_BGDWHITE "\x1B[91;107m"
+
+// Display the LIGHT RED text with LEMON background
+#define LRED_LEMON "\x1B[91;103m"
+
+// Display the LIGHT RED text with MEDIUM PINK background
+#define LRED_BGMPINK "\x1B[91;104m"
+
+// Display the LIGHT RED text with DARK PINK background
+#define LRED_BGDPINK "\x1B[91;105m"
+
+// Display the LIGHT RED text with LIGHT PINK background
+#define LRED_BGLPINK "\x1B[91;106m"
+
 
 /********************
  *      DRED
  ********************/
-#define DRED "\x1B[31m"  		                        // DARK RED COLOR
+// Display the DARK RED text with DARK GREEN background
+#define DRED_BGDGREEN "\x1B[31;40m"
+
+// Display the DARK RED text with LIGHT GREEN background
+#define DRED_BGLGREEN "\x1B[31;42m"
+
+// Display the DARK RED text with MEDIUM GREEN background
+#define DRED_BGMGREEN "\x1B[31;100m"
+
+// Display the DARK RED text with DARK RED background
+#define DRED_BGDRED "\x1B[31;41m"
+
+// Display the DARK RED text with LIGHT RED background
+#define DRED_BGLRED "\x1B[31;101m"
+
+// Display the DARK RED text with DARK ORANGE background
+#define DRED_BGDORANGE "\x1B[31;43m"
+
+// Display the DARK RED text with LIGHT ORANGE background
+#define DRED_BGLORANGE "\x1B[31;102m"
+
+// Display the DARK RED text with LIGHT BLUE background
+#define DRED_BGLBLUE "\x1B[31;44m"
+
+// Display the DARK RED text with LIGHT WHITE background
+#define DRED_BGLWHITE "\x1B[31;47m"
+
+// Display the DARK RED text with DARK WHITE background
+#define DRED_BGDWHITE "\x1B[31;107m"
+
+// Display the DARK RED text with LEMON background
+#define DRED_BGLEMON "\x1B[31;103m"
+
+// Display the DARK RED text with MEDIUM PINK background
+#define DRED_BGMPINK "\x1B[31;104m"
+
+// Display the DARK RED text with DARK PINK background
+#define DRED_BGDPINK "\x1B[31;105m"
+
+// Display the DARK RED text with LIGHT PINK background
+#define DRED_BGLPINK "\x1B[31;106m"
+
 
 /********************
  *      LWHITE
  ********************/
-#define LWHITE "\x1B[37m" 		                        // LIGHT WHITE COLOR
+// Display the LIGHT WHITE text with  background
+#define LWHITE "\x1B[37m"
 
 /********************
  *      DWHITE
  ********************/
-#define DWHITE "\x1B[97m"                               // DARK WHITE COLOR
+// Display the DARK WHITE text with  background
+#define DWHITE "\x1B[97m"
 
 /********************
  *      LBLUE
  ********************/
-#define LBLUE "\x1B[94m"                                // LIGHT BLUE COLOR
+// Display the LIGHT BLUE text with  background
+#define LBLUE "\x1B[94m" 
 
 /********************
  *      DBLUE
  ********************/
-#define DBLUE "\x1B[34m" 		                        // DARK BLUE COLOR
+// Display the DARK BLUE text with  background
+#define DBLUE "\x1B[34m"
 
 /********************
  *      LCYAN
  ********************/
-#define LCYAN "\x1B[96m"                                // LIGHT CYAN COLOR
+// Display the LIGHT CYAN text with  background
+#define LCYAN "\x1B[96m" 
 
 /********************
  *      DCYAN
  ********************/
-#define DCYAN "\x1B[36m" 		                        // DARK CYAN COLOR
+// Display the DARK CYAN text with  background
+#define DCYAN "\x1B[36m"
 
 /********************
  *      ORANGE
  ********************/
-#define ORANGE "\x1B[33m" 		                     // ORANGE COLOR
+// Display the ORANGE text with  background
+#define ORANGE "\x1B[33m"
 
 /********************
  *      BLACK
  ********************/
-#define BLACK "\x1B[30m" 		                        // BLACK COLOR
+// Display the BLACK text with  background
+#define BLACK "\x1B[30m"
 
 /********************
  *      MAGENTA
  ********************/
-#define MAGENTA "\x1B[35m" 	                            // MAGENTA COLOR
+// Display the MAGENTA text with  background
+#define MAGENTA "\x1B[35m"
 
 /********************
  *      YELLOW
  ********************/
-#define YELLOW "\x1B[92m"                               // YELLOW COLOR
+// Display the YELLOW text with  background
+#define YELLOW "\x1B[92m"
 
 /********************
  *      LEMON
  ********************/
-#define LEMON "\x1B[93m"                                // LEMON COLOR
+// Display the LEMON text with  background
+#define LEMON "\x1B[93m"
 
 /********************
  *      PINK
  ********************/
-#define PINK "\x1B[95m"                                 // PINK COLOR
+// Display the PINK text with  background
+#define PINK "\x1B[95m"
